b86add835aad1d73b8f91a4c949ff4c4e608160c
rohdef <rohdef+virk@rohdef.dk>
2025-06-12 11:55:29 +0000

    Updating deployment

--- dist/generate-gltf-structure.cjs
+++ dist/generate-gltf-structure.cjs
@@ -1,8 +1,32 @@
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+
 // generate-gltf-structure.ts
-import * as fs from "fs";
-import Path from "path";
-import { GLTFLoader } from "node-three-gltf";
-import { Group, Mesh, Object3D } from "three";
+var fs = __toESM(require("fs"), 1);
+var import_node_path = __toESM(require("path"), 1);
+var import_node_three_gltf = require("node-three-gltf");
+var import_three = require("three");
 if (process.argv.length != 4) {
   console.error("Usage: generateClasses [gltfPath] [outputPath]");
   console.error("    [gltfPath]   - the glb/gltf file to parse");
@@ -12,9 +36,9 @@ var gltfPath = process.argv[2];
 var outputPath = process.argv[3];
 parseGltf(gltfPath, outputPath);
 function parseGltf(gltfPath2, outputPath2) {
-  const loader = new GLTFLoader();
+  const loader = new import_node_three_gltf.GLTFLoader();
   const fileData = fs.readFileSync(gltfPath2);
-  const outputFilename = Path.basename(gltfPath2);
+  const outputFilename = import_node_path.default.basename(gltfPath2);
   loader.parse(
     fileData,
     "",
@@ -41,13 +65,13 @@ ${structureCode}
 function dumpObject3d(object3d, indent = "  ") {
   let element;
   switch (object3d.constructor) {
-    case Object3D:
+    case import_three.Object3D:
       element = `${indent}element: (contained: Object3D) => (contained.getObjectByName("${object3d.name}") as Object3D),`;
       break;
-    case Group:
+    case import_three.Group:
       element = `${indent}element: (contained: Object3D) => (contained.getObjectByName("${object3d.name}") as Group),`;
       break;
-    case Mesh:
+    case import_three.Mesh:
       element = `${indent}element: (contained: Object3D) => (contained.getObjectByName("${object3d.name}") as Mesh),`;
       break;
     default:
